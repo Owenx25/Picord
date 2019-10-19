267a194e17e8b3d7c1b610b02d0d16f18d06f98e
Owen McCormack <Owen_McCormack@student.uml.edu>
2019-10-19 17:52:37 -0400

    Progress on camera integration, can toggle front or back and ratio

--- Picord/Components/CameraScreen.tsx
+++ Picord/Components/CameraScreen.tsx
@@ -1,37 +1,121 @@
-import React from 'react';
-import { StyleSheet, View, Button, Image, TouchableHighlight } from 'react-native';
+import React, { RefObject } from 'react';
+import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native';
 
-import Constants from 'expo-constants'
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import * as FileSystem from 'expo-file-system';
 import { Camera } from 'expo-camera';
+
+enum CameraType {
+    front = Camera.Constants.Type.front,
+    back = Camera.Constants.Type.back
+}
 
 type Props = typeof CameraScreen.defaultProps & {
 
 }
 
 type State = {
-    type: any
+    type: CameraType
+    ratio: string,
+    ratioIndex: number
 }
 
 export class CameraScreen extends React.Component<Props, State> {
+    private camera: React.RefObject<Camera>
+
     constructor(props: Props) {
         super(props);
+        this.camera = React.createRef();
         this.state = {
-            type: Camera.Constants.Type.back
+            type: CameraType.back,
+            ratio: '16:9',
+            ratioIndex: 0
         };
     }
 
     public static defaultProps = {
     }
+
+    static navigationOptions = {
+        header: null
+    };
+
+    _onChangeCameraType = () => {
+        this.setState({
+            type:
+                this.state.type === CameraType.back
+                ? CameraType.front
+                : CameraType.back
+        });
+    }
+
+    _onPressRatioToggle = () => {
+        this.getRatios()
+        .then((ratios) => {
+            this.setState({
+                ratio: ratios[this.state.ratioIndex]
+            })
+            this.state.ratioIndex === ratios.length - 1
+                ? this.setState({ ratioIndex: 0 })
+                : this.setState({ ratioIndex: this.state.ratioIndex + 1 })
+        });
+    }
+
+    getRatios = async (): Promise<Array<string>> => {
+        const ratios = await this.camera.current.getSupportedRatiosAsync();
+        return ratios;
+    };
+
+    canChangeRatios = (): boolean => {
+        return Platform.OS === 'android';
+    }
     
     render() {
       return (
         <View style={{ flex: 1 }}>
-            <Camera style={{ flex: 1}} type={this.state.type} >
-            
+            <Camera 
+                ref={this.camera}
+                style={{ flex: 1}} 
+                type={this.state.type} 
+                ratio={this.state.ratio}
+            >
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'transparent',
+                        flexDirection: 'row'
+                }}>
+                    <TouchableOpacity
+                        style={styles.CameraButton}
+                        onPress={this._onChangeCameraType}
+                    >
+                        <Text style={styles.CameraButtonText}>Flip</Text>
+                    </TouchableOpacity>
+                    { this.canChangeRatios  &&
+                        <TouchableOpacity
+                            style={styles.CameraButton}
+                            onPress={this._onPressRatioToggle}
+                        >
+                            <Text style={styles.CameraButtonText}>{this.state.ratio}</Text>
+                        </TouchableOpacity>
+                    }
+                </View>
             </Camera>
         </View>
     )}
   }
+  const styles = StyleSheet.create({
+    CameraButton: {
+        flex: 0.2,
+        marginBottom: 20,
+        marginLeft: 20, 
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 10
+    },
+    CameraButtonText: {
+        fontSize: 24,
+        padding: 5,
+        color: 'white'
+    }
+  });
